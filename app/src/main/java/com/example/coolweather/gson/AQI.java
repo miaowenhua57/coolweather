1a519419e3624c55c73dc82c2c97d9aabaa327f7
mwh <miaowenhua0@163.com>
2018-05-09 17:15:46 +0800

    完成显示天气功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by MWH on 2018/5/9.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
